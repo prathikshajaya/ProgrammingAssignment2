f58c31f8f82c76f269c62fb8d9e77a87c12a213f
Prathiksha Jaya <prathikshajaya@gmail.com>
2020-05-21 14:42:43 +0530

    formulae added

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,32 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+##they are done in partial fulfillment of the R programming course
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv<-NULL
+  set<-function(y){
+    x<<-y
+    inv<<-NULL
+  }
+  get<-function()x
+  
+  setinverse<-function(inverse)inv<<-inverse
+  getinverse<-function()inv
+  list(set=set, setinverse=setinverse, getinverse=getinverse)
 }
 
 
 ## Write a short comment describing this function
-
+##it helps to get the inverse from the cache
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-}
+  inv<-get$inverse()
+  if(!is.null(inv)){
+    message("getting cache data")
+    return(inv)
+  }
+  data<-x$get()
+  inv<-solve(data,...)
+  x$setinverse(inv)
+  inv
+  }
